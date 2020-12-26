5a16311df58379a2f5a01be45966810871878a28
Timrin <19280360+Timrin@users.noreply.github.com>
2020-12-26 16:20:40 +0100

    Define weather api endpoints, and other small changes
    Created the apigateway package
    Created a class each for the two apis, weather and season
    Defined the weather api endpoints
    Implicitly declared server port

---
+++ backend/src/main/java/apigateway/seasonAPI.java
@@ -0,0 +1,15 @@
+package apigateway;
+
+import static spark.Spark.*;
+
+
+public class seasonAPI {
+
+	/**
+	 * Method for initializing all of the season endpoints
+	 */
+	public static void initAPI() {
+
+	}
+
+}
